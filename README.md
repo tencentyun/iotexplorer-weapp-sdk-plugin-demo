cb6fca9261ba4e4565e77a7b4a78f0fc3fb0c03e
liusen <562035512@qq.com>
2021-01-08 15:40:55 +0800

    feat(trtc): 添加实时音视频拨打页面示例

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ pages/index
 
 ### 指定连接某个设备
 pages/connect
+
+### 进行实时音视频拨打通话
+pages/trtcCall
---
+++ miniprogram/pages/trtcCall/trtcCall.js
@@ -0,0 +1,27 @@
+/**
+ * 先将对应设备的 视频拨打状态/语音拨打状态改为1
+ * 然后携带accessToken、type(audio/video), deviceId三个参数跳转到插件页面即可
+ */
+
+Page({
+  data: {
+    // deviceId 为 `${productId}/${devicename}`的形式
+    deviceId: '1P8YLxxxx/devxxxx'
+  },
+  onLoad: function () {
+  },
+  async getAccessToken () {
+    //获取accessToken逻辑
+    return ''
+  },
+  async redirectCallPage(type) {
+    const accessToken = await getAccessToken();
+    wx.navigateTo({ url: `plugin://iotexplorer-weapp-sdk-plugin/trtcCall?accessToken=${accessToken}&type=${type}&deviceId=${this.data.deviceId}`});
+  },
+  onVideoBtnClick: function () {
+    this.redirectCallPage(callTypeMap['video'])
+  },
+  onAudioBtnClick: function () {
+    this.redirectCallPage(callTypeMap['audio'])
+  }
+});
